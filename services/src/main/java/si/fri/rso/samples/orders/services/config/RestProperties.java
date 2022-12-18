cd189743ee7450d0e197105e233e299b76856e3f
nikolayVv <nikolaivalvasilev@gmail.com>
2022-12-18 17:18:36 +0100

    Upravljanje s konfiguracijo

---
+++ services/src/main/java/si/fri/rso/samples/orders/services/config/RestProperties.java
@@ -0,0 +1,22 @@
+package si.fri.rso.samples.orders.services.config;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+import com.kumuluz.ee.configuration.cdi.ConfigValue;
+
+@ConfigBundle("rest-properties")
+@ApplicationScoped
+public class RestProperties {
+
+    @ConfigValue(watch = true)
+    private Boolean maintenanceMode;
+
+    public Boolean getMaintenanceMode() {
+        return this.maintenanceMode;
+    }
+
+    public void setMaintenanceMode(final Boolean maintenanceMode) {
+        this.maintenanceMode = maintenanceMode;
+    }
+}
