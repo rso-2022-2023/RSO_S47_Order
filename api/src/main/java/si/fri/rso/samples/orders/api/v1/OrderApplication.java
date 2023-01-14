dd1bd1c0ff6fce00548f36e591c33e774edc2317
nikolayVv <nikolaivalvasilev@gmail.com>
2023-01-14 02:08:02 +0100

    external IP

--- api/src/main/java/si/fri/rso/samples/orders/api/v1/OrderApplication.java
+++ api/src/main/java/si/fri/rso/samples/orders/api/v1/OrderApplication.java
@@ -14,7 +14,7 @@ import org.eclipse.microprofile.openapi.annotations.servers.Server;
 @OpenAPIDefinition(info = @Info(title = "Orders API", version = "v1",
         contact = @Contact(email = "nv7834@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing orders."),
-        servers = @Server(url = "http://localhost:8080/"),
+        servers = @Server(url = "http://146.190.206.59:8080/"),
         externalDocs = @ExternalDocumentation(description = "Order application repository", url = "https://github.com/rso-2022-2023/RSO_S47_Order"))
 @ApplicationPath("/v1")
 public class OrderApplication extends Application {
